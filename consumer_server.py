744169136e76580455060d3ab4a63e05a90e796d
Pradeep <ppujari@github.com>
2019-11-09 12:49:13 -0800

    added ip

--- consumer_server.py
+++ consumer_server.py
@@ -10,7 +10,7 @@ consumer = topic.get_balanced_consumer(
     consumer_group=b'pytkafka-test-2',
     auto_commit_enable=False,
     auto_offset_reset=OffsetType.EARLIEST,
-    zookeeper_connect='remotehost:2181'
+    zookeeper_connect='192.168.1.10:2181'
 )
 for message in consumer:
     if message is not None:
